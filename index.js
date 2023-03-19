32c237fc593137ac91fa24454ec6ef005a69cc38
cradle0fFilth <64477775+cradle0fFilth@users.noreply.github.com>
2023-03-19 19:12:21 +0800

    Update index.js

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ require('dotenv').config();
 const salt = bcrypt.genSaltSync(10);
 const secret = "hiqhwiqwoqkwpq"
 
-//app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
-app.use(cors({ credentials: true, origin: 'https://cradle0ffilth.github.io' }));
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+//app.use(cors({ credentials: true, origin: 'https://cradle0ffilth.github.io' }));
 app.use(express.json());
 app.use(cookiePasrser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
